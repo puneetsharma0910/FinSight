bae6a23e54af30aa569e0587d9258c18be549dd9
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-06-07 02:41:57 +0530

    - Add schema definition and create account drawer - Add Sonner UI component and use-fetch hook - Update package.json and package-lock.json

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -5,6 +5,7 @@ const DashboardLayout = () => {
   return (
     <div className="px-5">
       <h1 className="text-6xl font-bold gradient-title mb-5">Dashboard</h1>
+      {/* dashboard page */}
       <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea"/>}>
         <DashboardPage />
       </Suspense>
