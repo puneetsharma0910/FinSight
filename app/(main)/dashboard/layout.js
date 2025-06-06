e286b706b10feaf925d2b0fb4575efee5db8c720
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-06-06 23:52:03 +0530

    Add main dashboard, account, and transaction pages

---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,15 @@
+import React, { Suspense } from "react";
+import {BarLoader} from "react-spinners";
+import DashboardPage from "./page";
+const DashboardLayout = () => {
+  return (
+    <div className="px-5">
+      <h1 className="text-6xl font-bold gradient-title mb-5">Dashboard</h1>
+      <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea"/>}>
+        <DashboardPage />
+      </Suspense>
+    </div>
+  );
+};
+
+export default DashboardLayout;
