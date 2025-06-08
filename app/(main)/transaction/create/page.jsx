34af4e1fa9600c04abb47b099564d683089aa553
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-06-09 01:29:59 +0530

    Added transaction form and update transaction creation page

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,8 +1,20 @@
+import { getUserAccounts } from '@/actions/dashboard'
 import React from 'react'
+import AddTransactionForm from '../_components/transaction-form';
+import { defaultCategories } from '@/data/categories';
 
-const AddTransactionPage = () => {
+const AddTransactionPage = async () => {
+    const accounts = await getUserAccounts();
   return (
-    <div>AddTransactionPage</div>
+    <div className='max-w-3xl mx-auto px-5'>
+        <h1 className='text-5xl gradient-title mb-8'>
+            Add Transaction
+        </h1>
+
+        <AddTransactionForm accounts={accounts} categories={defaultCategories}/>
+
+
+    </div>
   )
 }
 
