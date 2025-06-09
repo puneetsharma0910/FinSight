d8d1badd8b825ca28a1e99902d1f2a46ecb35024
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-06-09 18:05:43 +0530

    Refactor inngest integration: update route and functions for improved event handling

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,14 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts } from "@/lib/inngest/functions";
+import { checkBudgetAlerts, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
 
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     checkBudgetAlerts,
+    triggerRecurringTransactions,
+    processRecurringTransaction
     // Add other functions when they're implemented
   ],
 });
